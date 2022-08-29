bfe4a8f3f0aa762bca70f69a47944a716be50a1b
Avrel Owona <guyowona01@gmail.com>
2022-08-29 17:06:01 +0100

    g

---
+++ src/context/userContext.js
@@ -0,0 +1,4 @@
+import {createContext} from "react";
+
+//On crée le context user utilisateur avant de l'importer dans index avec le provider et l'on l'initie a null
+export const UserContext = createContext(null)
