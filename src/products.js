bb0fd9fcb7b124d0072d812880dee5f3cf6441f7
Avrel Owona <guyowona01@gmail.com>
2022-09-03 15:22:04 +0100

    axios request

--- src/products.js
+++ src/products.js
@@ -1,26 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {Link} from "react-router-dom";
+import {useAuth} from "./context/authContext";
 
 export function Products() {
     const [products, setProducts] = useState([])
+    const [error, setError] = useState([])
+
 
     // const {user}= useAuthContext()
     // console.log('currentUser', user)
 
     const getAllProducts = async () => {
         try {
+            // Lorsqu'on utilise un try catch dans la fonction, on peut se passer de then
             const response = await axios.get('https://fakestoreapi.com/products')
             console.log('response', response)
             setProducts(response.data)
         } catch (e) {
             console.log('error', e)
         }
-        // const response  = await axios.get('https://fakestoreapi.com/products')
-        //     .catch((err) => {
-        //         console.log('err', err)
-        //     })
-        // setProducts(response.data)
     }
     useEffect(()=> {
         getAllProducts()
@@ -35,14 +34,16 @@ export function Products() {
                     <div className='flex flex-wrap justify-center mt-40'>
                         {products.map((product)=> {
                             return (
-                                <div className="w-80 border flex flex-col items-center pb-8 carte mt-8 mx-2">
-                                    <div className="h-64 w-full border">
-                                        <img src={product.image} className="w-full h-full object-cover" alt=""/></div>
-                                    <h3 className="text-center text-2xl text-gray-700 font-medium pt-8">
-                                        {product.title}
-                                    </h3><span
-                                    className="text-gray-400 text-center text-lg font-medium pt-4 font-light">{product.price} $</span>
-                                </div>
+                                <Link to={product.id}>
+                                    <div className="w-80 border flex flex-col items-center pb-8 carte mt-8 mx-2">
+                                        <div className="h-64 w-full border">
+                                            <img src={product.image} className="w-full h-full object-cover" alt=""/></div>
+                                        <h3 className="text-center text-2xl text-gray-700 font-medium pt-8">
+                                            {product.title}
+                                        </h3><span
+                                        className="text-gray-400 text-center text-lg font-medium pt-4 font-light">{product.price} $</span>
+                                    </div>
+                                </Link>
                             )
                         })}
                     </div>
